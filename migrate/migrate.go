bb654ba77986a83bf3895fd11fd52c23d3d5931e
Azzam Syakir <azzamsykir@gmail.com>
2024-02-20 05:39:39 +0700

    change pkg migration to migrate

---
+++ migrate/migrate.go
@@ -0,0 +1,25 @@
+package migrate
+
+import (
+	"database/sql"
+
+	"github.com/rs/zerolog/log"
+)
+
+func MigrationDb(db *sql.DB) error {
+	// Memeriksa koneksi ke database
+	err := db.Ping()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Gagal melakukan ping ke database")
+	}
+	// Panggil fungsi migrateuntuk inisialisasi migrasi database
+	if err := Usermigration(db); err != nil {
+		log.Error().Err(err).Msg("Gagal melakukan migrasi user")
+	}
+
+	if err := Tokenmigration(db); err != nil {
+		log.Error().Err(err).Msg("Gagal melakukan migrasi token")
+	}
+
+	return err
+}
