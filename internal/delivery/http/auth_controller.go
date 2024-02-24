947f9a1bc1e2c16e4475c1ffc173d677c0226c5f
Asa <azzamsykir@gmail.com>
2024-02-24 20:44:55 +0700

    move logout to auth

--- internal/delivery/http/auth_controller.go
+++ internal/delivery/http/auth_controller.go
@@ -83,3 +83,30 @@ func (c *UserController) Login(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(response)
 }
+
+func (c *UserController) Logout(w http.ResponseWriter, r *http.Request) {
+	// Get the token from the "Authorization" header
+	tokenString := r.Header.Get("Authorization")
+
+	// Remove "Bearer " from the token string
+	tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
+
+	// Call the LogoutUser function from the service
+	err := c.UseCase.Logout(tokenString) //delete token in redis
+	if err != nil {
+		errorMessage := fmt.Sprintf("Failed to logout user: %v", err)
+		responses.ErrorResponse(w, errorMessage, http.StatusInternalServerError)
+		return
+	}
+	// Delete RefToken from cookie
+	expiration := time.Now().AddDate(0, 0, -1) // Set the expiration time to the past
+	cookie := http.Cookie{
+		Name:    "token",
+		Value:   "",
+		Expires: expiration,
+	}
+	http.SetCookie(w, &cookie)
+
+	// Return a success message after logout
+	responses.OtherResponses(w, "Logout successful", http.StatusOK)
+}
--- internal/delivery/http/user_controller.go
+++ internal/delivery/http/user_controller.go
@@ -159,29 +159,4 @@ func (c *UserController) Delete(w http.ResponseWriter, r *http.Request) {
 }
 
 
-func (c *UserController) Logout(w http.ResponseWriter, r *http.Request) {
-	// Get the token from the "Authorization" header
-	tokenString := r.Header.Get("Authorization")
 
-	// Remove "Bearer " from the token string
-	tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
-
-	// Call the LogoutUser function from the service
-	err := c.UseCase.Logout(tokenString) //delete token in redis
-	if err != nil {
-		errorMessage := fmt.Sprintf("Failed to logout user: %v", err)
-		responses.ErrorResponse(w, errorMessage, http.StatusInternalServerError)
-		return
-	}
-	// Delete RefToken from cookie
-	expiration := time.Now().AddDate(0, 0, -1) // Set the expiration time to the past
-	cookie := http.Cookie{
-		Name:    "token",
-		Value:   "",
-		Expires: expiration,
-	}
-	http.SetCookie(w, &cookie)
-
-	// Return a success message after logout
-	responses.OtherResponses(w, "Logout successful", http.StatusOK)
-}
