8815159efb90b047200d994a5c4d4f449295051a
Asa <azzamsykir@gmail.com>
2024-02-24 20:44:36 +0700

    move login to auth

--- internal/delivery/http/user_controller.go
+++ internal/delivery/http/user_controller.go
@@ -158,52 +158,6 @@ func (c *UserController) Delete(w http.ResponseWriter, r *http.Request) {
 	responses.OtherResponses(w, "Success delete user", http.StatusOK)
 }
 
-func (c *UserController) Login(w http.ResponseWriter, r *http.Request) {
-	var requestUser map[string]string
-
-	// Read JSON from request body
-	if err := json.NewDecoder(r.Body).Decode(&requestUser); err != nil {
-		responses.ErrorResponse(w, "Failed to read user data from request", http.StatusBadRequest)
-		return
-	}
-
-	// Get email and password from the user data
-	email, ok := requestUser["email"]
-	if !ok {
-		responses.ErrorResponse(w, "Email must be provided", http.StatusBadRequest)
-		return
-	}
-
-	password, ok := requestUser["password"]
-	if !ok {
-		responses.ErrorResponse(w, "Password must be provided", http.StatusBadRequest)
-		return
-	}
-
-	// Call the usecase to perform login
-	token, err := c.UseCase.Login(email, password)
-	if err != nil {
-		errorMessage := fmt.Sprintf("Login failed: %v", err)
-
-		responses.ErrorResponse(w, errorMessage, http.StatusUnauthorized)
-		return
-	}
-
-	// Add token to cookie
-	expiration := time.Now().Add(7 * 24 * time.Hour) // Expiration 7 days
-	cookie := http.Cookie{
-		Name:    "token",
-		Value:   token,
-		Expires: expiration,
-	}
-	http.SetCookie(w, &cookie)
-
-	// Respond with the token
-	response := map[string]interface{}{
-		"token": token,
-	}
-	json.NewEncoder(w).Encode(response)
-}
 
 func (c *UserController) Logout(w http.ResponseWriter, r *http.Request) {
 	// Get the token from the "Authorization" header
