0bd16ec3ab99742684b599ef31324b9c0e93b63d
Asa <azzamsykir@gmail.com>
2024-02-24 20:46:32 +0700

    move register to auth_repo

--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -1,1 +1,11 @@
 package repository
+
+func (ur *UserRepository) Register(id, name, email, hashedPassword string, createdAt, updatedAt time.Time) error {
+	createSQL := `
+	    INSERT INTO users (id, name, email, password, created_at, updated_at)
+	    VALUES (?, ?, ?, ?, CONVERT_TZ(?, '+00:00', '+07:00'), CONVERT_TZ(?, '+00:00', '+07:00'))
+	`
+
+	_, err := ur.db.Exec(createSQL, id, name, email, hashedPassword, createdAt.UTC(), updatedAt.UTC())
+	return err
+}
