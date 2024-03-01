e0dbe954cc3a9b49859ad08de6f9cdf3384424b2
Asa <azzamsykir@gmail.com>
2024-03-01 14:34:08 +0700

    fix(repository): fix register function

--- internal/repository/auth_repository.go
+++ internal/repository/auth_repository.go
@@ -8,41 +8,12 @@ type AuthRepository struct {
 func NewAuthRepository(db *sql.DB) *AuthRepository {
 	return &AuthRepository{db: db}
 }
+func (ur *UserRepository) Register(id, name, email, hashedPassword string, createdAt, updatedAt time.Time) error {
+	createSQL := `
+	    INSERT INTO users (id, name, email, password, created_at, updated_at)
+	    VALUES (?, ?, ?, ?, CONVERT_TZ(?, '+00:00', '+07:00'), CONVERT_TZ(?, '+00:00', '+07:00'))
+	`
 
-func (c *UserController) Register(w http.ResponseWriter, r *http.Request) {
-	var user struct {
-		ID       string `json:"id"`
-		Name     string `json:"name"`
-		Email    string `json:"email"`
-		Password string `json:"password"`
-	}
-
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
-		responses.ErrorResponse(w, "Failed to read user data from the request", http.StatusBadRequest)
-		return
-	}
-
-	err := c.UseCase.Register(user.ID, user.Name, user.Email, user.Password)
-	if err != nil {
-		errorMessage := fmt.Sprintf("Failed to create user: %v", err)
-		responses.ErrorResponse(w, errorMessage, http.StatusInternalServerError)
-		return
-	}
-
-	currentTime := time.Now()
-
-	// Create a user data object to be sent in the response
-	userData := struct {
-		Name      string    `json:"name"`
-		Email     string    `json:"email"`
-		CreatedAt time.Time `json:"created_at"`
-		UpdatedAt time.Time `json:"updated_at"`
-	}{
-		Name:      user.Name,
-		Email:     user.Email,
-		CreatedAt: currentTime,
-		UpdatedAt: currentTime,
-	}
-
-	responses.SuccessResponse(w, "Success", userData, http.StatusCreated)
+	_, err := ur.db.Exec(createSQL, id, name, email, hashedPassword, createdAt.UTC(), updatedAt.UTC())
+	return err
 }
